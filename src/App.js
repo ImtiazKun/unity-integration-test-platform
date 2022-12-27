d402e526ea12c10684e65e23b886e56c449c8389
imtiazkun <alshahriar.me@gmail.com>
2022-12-27 15:59:52 +0600

    Added 1 game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import { Fragment, useEffect, useState, useCallback } from "react";
 
 function App() {
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [userName, setUserName] = useState();
+  const [score, setScore] = useState();
+
+  const {
+    unityProvider,
+    sendMessage,
+    isLoaded,
+    addEventListener,
+    removeEventListener,
+  } = useUnityContext({
+    loaderUrl: "games/eleven-eleven/eleveneleven.loader.js",
+    dataUrl: "games/eleven-eleven/eleveneleven.data",
+    frameworkUrl: "games/eleven-eleven/eleveneleven.framework.js",
+    codeUrl: "games/eleven-eleven/eleveneleven.wasm",
+  });
+
+  // Changes Scene
+  function loadScene() {
+    sendMessage("LoadLevelManager", "LoadLevel", 0);
+  }
+
+  // Game Over Handler
+  const handleGameOver = useCallback((score) => {
+    setIsGameOver(true);
+    setScore(score);
+  }, []);
+
+  useEffect(() => {
+    loadScene();
+  }, [isLoaded]);
+
+  useEffect(() => {
+    addEventListener("GameOver", handleGameOver);
+    return () => {
+      removeEventListener("GameOver", handleGameOver);
+    };
+  }, [addEventListener, removeEventListener, handleGameOver]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        width: "100vw",
+        flexDirection: "column-reverse"
+      }}
+    >
+      <Fragment>
+        <Unity
+          style={{
+            width: 1600,
+            height: 900,
+            visibility: isLoaded ? "visible" : "hidden",
+          }}
+          unityProvider={unityProvider}
+        />
+        <div>
+          {isGameOver === true && <p>{`You've scored ${score} points.`}</p>}
+        </div>
+      </Fragment>
     </div>
   );
 }
